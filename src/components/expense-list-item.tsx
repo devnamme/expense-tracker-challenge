2e8ad9cdd91228de26f963a0f268fcb17954b643
Emman Evangelista <emmanevangelista129@gmail.com>
2024-07-14 00:09:46 +0800

    add edit icon, add styling in list item

--- src/components/expense-list-item.tsx
+++ src/components/expense-list-item.tsx
@@ -1,3 +1,4 @@
+import { More } from "iconsax-react";
 import { MouseEventHandler } from "react";
 import { useDispatch } from "react-redux";
 import { setEditing } from "../redux/modules/expense-modal";
@@ -27,7 +28,7 @@ const MONTHS = [
 export default function ExpenseListItem({ expense, showDate = false }: Props) {
   const dispatch = useDispatch<AppDispatch>();
 
-  const onListItemClick: MouseEventHandler = (event) => {
+  const onEditClick: MouseEventHandler = () => {
     dispatch(setEditing({ expense: expense }));
   };
 
@@ -39,17 +40,22 @@ export default function ExpenseListItem({ expense, showDate = false }: Props) {
     }`;
 
   return (
-    <div
-      className="bg-gray-light py-2 px-4 rounded flex flex-row flex-nowrap items-center justify-between cursor-pointer"
-      onClick={onListItemClick}
-    >
-      <p className="">{expense.name}</p>
-      <div className="flex flex-col items-end">
-        <p className="font-medium">PHP {expense.amount.toFixed(2)}</p>
+    <div className="bg-gray-light py-2 px-4 rounded flex flex-row flex-nowrap items-center justify-start gap-x-4">
+      <p className="mr-auto shrink line-clamp-2">{expense.name}</p>
+
+      <div className="shrink-0 flex flex-col items-end">
+        <p className="font-medium text-right">
+          PHP {expense.amount.toFixed(2)}
+        </p>
         <p className={`font-light ${showDate ? "" : "hidden"}`}>
           {formatDate(new Date(expense.date))}
         </p>
       </div>
+
+      <More
+        className="shrink-0 rotate-90 cursor-pointer"
+        onClick={onEditClick}
+      />
     </div>
   );
 }
