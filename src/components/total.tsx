4e380f346737b70344ea468196e19a3c4c3e4daf
Emman Evangelista <emmanevangelista129@gmail.com>
2024-07-14 05:13:59 +0800

    set total align to right

--- src/components/total.tsx
+++ src/components/total.tsx
@@ -6,7 +6,7 @@ interface Props {
 export default function Total({ value, className = "" }: Props) {
   return (
     <div className={`bg-primary rounded-md text-white p-4 ${className}`}>
-      <p className="text-4xl sm:text-5xl md:text-6xl font-medium text-left">
+      <p className="text-4xl sm:text-5xl md:text-6xl font-medium text-right">
         PHP{" "}
         {value.toLocaleString(undefined, {
           minimumFractionDigits: 2,
