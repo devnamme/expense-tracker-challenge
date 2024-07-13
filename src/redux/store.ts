dc5b86861362ea72353e0af376244dc3e18c7c8f
Emman Evangelista <emmanevangelista129@gmail.com>
2024-07-13 08:21:13 +0800

    typo

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -10,4 +10,4 @@ export const store = configureStore({
 });
 
 export type AppDispatch = typeof store.dispatch;
-export type RootSTate = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>;
