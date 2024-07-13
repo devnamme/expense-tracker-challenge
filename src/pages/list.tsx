121814f9a34e8b00b59e9c68424e1c5509900c54
Emman Evangelista <emmanevangelista129@gmail.com>
2024-07-13 23:10:55 +0800

    sort expenses

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -15,15 +15,19 @@ export default function ListPage() {
   return (
     <div className="min-h-0 grow flex flex-col flex-nowrap gap-y-2 overflow-y-auto -mb-6 pb-6">
       {expenses[getGroup()][pagination.date] !== undefined ? (
-        expenses[getGroup()][pagination.date].expenses.map(
-          (expenseID: number) => (
+        [...expenses[getGroup()][pagination.date].expenses]
+          .sort((expenseIDA: number, expenseIDB: number) =>
+            expenses.byID[expenseIDA].date.localeCompare(
+              expenses.byID[expenseIDB].date
+            )
+          )
+          .map((expenseID: number) => (
             <ExpenseListItem
               key={`expense-list-item-${expenses.byID[expenseID].id}`}
               expense={expenses.byID[expenseID]}
               showDate={pagination.mode !== "day"}
             />
-          )
-        )
+          ))
       ) : (
         <p>No expenses found!</p>
       )}
