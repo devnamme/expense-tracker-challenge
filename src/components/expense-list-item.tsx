8dff945cda22b766c84e7646e643d62e42d7ddc7
Emman Evangelista <emmanevangelista129@gmail.com>
2024-07-13 22:57:49 +0800

    add new styles to list item

--- src/components/expense-list-item.tsx
+++ src/components/expense-list-item.tsx
@@ -9,6 +9,21 @@ interface Props {
   showDate?: boolean;
 }
 
+const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 export default function ExpenseListItem({ expense, showDate = false }: Props) {
   const dispatch = useDispatch<AppDispatch>();
 
@@ -16,15 +31,24 @@ export default function ExpenseListItem({ expense, showDate = false }: Props) {
     dispatch(setEditing({ expense: expense }));
   };
 
+  const formatDate = (date: Date) =>
+    `${MONTHS[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}, ${
+      date.getHours() > 12 ? date.getHours() - 12 : date.getHours()
+    }:${date.getMinutes().toString().padStart(2, "0")} ${
+      date.getHours() >= 12 ? "PM" : "AM"
+    }`;
+
   return (
     <div
-      className="bg-gray-light p-2 rounded flex flex-row flex-nowrap items-center justify-between cursor-pointer"
+      className="bg-gray-light py-2 px-4 rounded flex flex-row flex-nowrap items-center justify-between cursor-pointer"
       onClick={onListItemClick}
     >
       <p className="">{expense.name}</p>
       <div className="flex flex-col items-end">
-        <p className="">PHP {expense.amount.toFixed(2)}</p>
-        <p className={showDate ? "" : "hidden"}>{expense.date}</p>
+        <p className="font-medium">PHP {expense.amount.toFixed(2)}</p>
+        <p className={`font-light ${showDate ? "" : "hidden"}`}>
+          {formatDate(new Date(expense.date))}
+        </p>
       </div>
     </div>
   );
