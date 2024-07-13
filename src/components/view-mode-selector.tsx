7ce43b8563e47b736613b1fd9373d18ef28daac3
Emman Evangelista <emmanevangelista129@gmail.com>
2024-07-13 23:03:38 +0800

    change color of view mode selector buttons

--- src/components/view-mode-selector.tsx
+++ src/components/view-mode-selector.tsx
@@ -31,7 +31,7 @@ export default function ViewModeSelector({
   return (
     <div className={`flex flex-row flex-nowrap gap-x-4 shrink-1 ${className}`}>
       <select
-        className="bg-gray-light rounded-md px-4 min-w-0 shrink-1 mr-auto appearance-none cursor-pointer"
+        className="bg-gray-dark text-white rounded-md px-4 min-w-0 shrink-1 mr-auto appearance-none cursor-pointer"
         value={mode}
         onChange={(event) => setMode(event.target.value as ViewMode)}
       >
@@ -41,21 +41,21 @@ export default function ViewModeSelector({
       </select>
 
       <button
-        className="bg-gray-light rounded-md h-12 w-12 flex flex-col items-center justify-center shrink-0"
+        className="bg-gray-dark text-white rounded-md h-12 w-12 flex flex-col items-center justify-center shrink-0"
         onClick={onLeftClick}
       >
         <ArrowLeft2 size="1rem" />
       </button>
 
       <input
-        className="bg-gray-light rounded-md px-4 min-w-0 shrink-1 appearance-none cursor-pointer"
+        className="bg-gray-dark text-white invert-date-icon rounded-md px-4 min-w-0 shrink-1 appearance-none cursor-pointer"
         type={mode === "day" || mode === "week" ? "date" : mode}
         value={date}
         onChange={onDateChange}
       />
 
       <button
-        className="bg-gray-light rounded-md h-12 w-12 flex flex-col items-center justify-center shrink-0"
+        className="bg-gray-dark text-white rounded-md h-12 w-12 flex flex-col items-center justify-center shrink-0"
         onClick={onRightClick}
       >
         <ArrowRight2 size="1rem" />
