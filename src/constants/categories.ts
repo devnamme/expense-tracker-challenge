81e79f14feaecf29021e2ca1081e476980741d5e
Emman Evangelista <emmanevangelista129@gmail.com>
2024-07-13 16:43:13 +0800

    fix typo

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -1,7 +1,7 @@
 export const Categories = [
   { name: "Food", value: "food" },
   { name: "Bills", value: "bills" },
-  { name: "Transporation", value: "transportation" },
+  { name: "Transportation", value: "transportation" },
   { name: "Clothing", value: "clothing" },
   { name: "Others", value: "others" },
 ];
